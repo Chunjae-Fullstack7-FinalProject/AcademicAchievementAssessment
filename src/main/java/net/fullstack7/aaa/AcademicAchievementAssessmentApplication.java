69dd7d832643c9debe7da8e7983c326d635962f8
gyeongmin <kgmksw@naver.com>
2024-12-16 16:59:39 +0900

    first commit

---
+++ src/main/java/net/fullstack7/aaa/AcademicAchievementAssessmentApplication.java
@@ -0,0 +1,13 @@
+package net.fullstack7.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicAchievementAssessmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AcademicAchievementAssessmentApplication.class, args);
+  }
+
+}
