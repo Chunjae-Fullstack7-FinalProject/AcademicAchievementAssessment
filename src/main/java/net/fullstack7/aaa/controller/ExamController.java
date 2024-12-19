354cf72b3aaacc81a1051cc57c0552c64c368cd5
HyperQuanx <qbixroqkfwk@gmail.com>
2024-12-19 15:14:34 +0900

    [Merge:dev] 2024

--- src/main/java/net/fullstack7/aaa/controller/ExamController.java
+++ src/main/java/net/fullstack7/aaa/controller/ExamController.java
@@ -1,5 +1,8 @@
 package net.fullstack7.aaa.controller;
 
+import lombok.RequiredArgsConstructor;
+import net.fullstack7.aaa.dto.exam.ItemList;
+import net.fullstack7.aaa.service.exam.ExamServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +15,11 @@ import net.fullstack7.aaa.common.annotation.Logging;
 @Logging
 @RequiredArgsConstructor
 public class ExamController {
-    private final ExamService examService;
+    private final ExamServiceImpl examService;
 
     @GetMapping("/{examId}")
     public String exam(@PathVariable Long examId) {
-        ItemList itemList = examService.getItemList(examId);
+//        ItemList itemList = examService.getItemList(examId);
         return "exam/exam";
     }
 }
